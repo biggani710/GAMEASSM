7f99b0e68a2463bd4700af12ecd912750a1c9cc9
DatFlorentino <nai.hoalan@gmail.com>
2024-11-29 16:39:25 +0700

    Initial commit

---
+++ Assets/MovementDisabler.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class MovementDisabler : StateMachineBehaviour
+{
+    private WeaponManager weaponmanager;
+    public enum Option
+    {
+        Enable, Disable
+    }
+    public Option enableMovement;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        weaponmanager = animator.GetComponent<WeaponManager>();
+        if (enableMovement == Option.Enable)
+        {
+            weaponmanager.EnableMovement(true);
+        }
+        else
+            weaponmanager.EnableMovement(false);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
